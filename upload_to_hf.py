7881173ea8744111b2fe4e1a20a5cf8733566413
Jun Park <pj2417@gmail.com>
2024-03-18 22:11:46 +0900

    no revision

--- upload_to_hf.py
+++ upload_to_hf.py
@@ -1,8 +1,5 @@
 from huggingface_hub import HfApi
-from sys import argv
 
-revision = argv[1]
-print(revision)
 api = HfApi()
 
 # Upload all the content from the local folder to your remote Space.
@@ -11,5 +8,4 @@ api.upload_folder(
     folder_path="./gemma-ko-2b-dev",
     repo_id="gemmathon/gemma-ko-2b-dev",
     repo_type="model",
-    revision=revision,
 )

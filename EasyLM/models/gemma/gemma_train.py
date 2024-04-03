f1d6c47a926208b7c1eeeaa2c9ab407253ac9502
wldud01 <yunjiyeong0106@gmail.com>
2024-04-03 23:09:29 +0900

    update

--- EasyLM/models/gemma/gemma_train.py
+++ EasyLM/models/gemma/gemma_train.py
@@ -127,7 +127,7 @@ def main(argv):
     def train_step(train_state, rng, batch):
         rng_generator = JaxRNG(rng)
         batch = with_sharding_constraint(batch, PS(("dp", "fsdp")))
-        print(train_state)
+        print(train_state.parmas.layers)
         def loss_and_accuracy(params):
             logits = model.apply(
                 params,

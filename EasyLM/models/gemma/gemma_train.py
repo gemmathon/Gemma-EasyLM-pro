d291cc58bfe37d97e06fac87d881e966b278bbc2
wldud01 <yunjiyeong0106@gmail.com>
2024-04-04 12:09:12 +0900

    update

--- EasyLM/models/gemma/gemma_train.py
+++ EasyLM/models/gemma/gemma_train.py
@@ -155,11 +155,11 @@ def main(argv):
         for k in train_state.params['params']['model']['layers'].keys():
             if k not in ['6','13','20']:
                 name = "default"
+                print(train_state.params['params']['model']['layers'][k])
                 train_state.params['params']['model']['layers'][k]['input_layernorm'][name] = train_state.params['params']['model']['layers'][k]['input_layernorm'].pop('weight')
-                train_state.params['params']['model']['layers'][k]['mlp'][name] = train_state.params['params']['model']['layers'][k]['input_layernorm'].pop('weight')
-                train_state.params['params']['model']['layers'][k]['input_layernorm']['down_proj'][name] = train_state.params['params']['model']['layers'][k]['input_layernorm']['down_proj'].pop('kernel')
-                train_state.params['params']['model']['layers'][k]['input_layernorm']['gate_proj'][name] = train_state.params['params']['model']['layers'][k]['input_layernorm']['gate_proj'].pop('kernel')
-                train_state.params['params']['model']['layers'][k]['input_layernorm']['up_proj'][name] = train_state.params['params']['model']['layers'][k]['input_layernorm']['up_proj'].pop('kernel')
+                train_state.params['params']['model']['layers'][k]['input_layernorm']['mlp']['down_proj'][name] = train_state.params['params']['model']['layers'][k]['input_layernorm']['mlp']['down_proj'].pop('weight')
+                train_state.params['params']['model']['layers'][k]['input_layernorm']['mlp']['gate_proj'][name] = train_state.params['params']['model']['layers'][k]['input_layernorm']['mlp']['gate_proj'].pop('weight')
+                train_state.params['params']['model']['layers'][k]['input_layernorm']['mlp']['up_proj'][name] = train_state.params['params']['model']['layers'][k]['input_layernorm']['mlp']['up_proj'].pop('weight')
                 train_state.params['params']['model']['layers'][k]['post_attention_layernorm'][name] = train_state.params['params']['model']['layers'][k]['post_attention_layernorm'].pop('weight')
                 train_state.params['params']['model']['layers'][k]['self_attn']['k_proj'][name] = train_state.params['params']['model']['layers'][k]['input_layernorm'].pop('kernel')
                 train_state.params['params']['model']['layers'][k]['self_attn']['o_proj'][name] = train_state.params['params']['model']['layers'][k]['input_layernorm'].pop('kernel')
